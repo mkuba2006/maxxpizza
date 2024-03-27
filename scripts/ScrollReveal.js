11d2e8eb32dd7b53aca0a1a894949a2b14022fe8
Jakub Myrta <mkuba2006@gmail.com>
2024-03-27 19:08:18 +0100

    27.03

---
+++ scripts/ScrollReveal.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('#main div p', { delay: 200, origin: 'bottom', distance: '30%',});
+ScrollReveal().reveal('#main div h4', { delay: 600, origin: 'bottom', distance: '30%',});
+ScrollReveal().reveal('#main div #dwa a', { delay: 1000, origin: 'bottom', distance: '30%', interval: 200});
--- scripts/nav.js
+++ scripts/nav.js
@@ -4,9 +4,7 @@ document.body.addEventListener("scroll", () => {
     var nav = document.getElementById("nav");
     if (document.body.scrollTop > 0) {
         nav.style.backgroundColor = '#000000c7';
-        console.log('w dół');
     } else {
         nav.style.backgroundColor = default_color;
-        console.log('w górę');
     }
 });
