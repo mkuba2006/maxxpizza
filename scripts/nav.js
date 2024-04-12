8479f0863af1f1c3c0934a00131f3560635d4585
Jakub Myrta <mkuba2006@gmail.com>
2024-04-12 23:03:50 +0200

    9

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,13 +3,12 @@ const extras_button = document.getElementById("extras_button");
 const defaultColor = window.getComputedStyle(nav).backgroundColor;
 const logo = document.getElementById("logo_img");
 
-if (window.innerWidth >"750px"){
+if (window.innerWidth >750){
     document.addEventListener("scroll", () => {
         if (window.scrollY > 300) {
             nav.style.backgroundColor = 'black';
             extras_button.style.backgroundColor = 'black';
             logo.style.backgroundColor = 'black';
-            logo.style.padding='14px 24px'
             logo.style.borderRadius='32px'
         } else {
             nav.style.backgroundColor = defaultColor;
