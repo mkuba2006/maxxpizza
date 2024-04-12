b607eaf57dd443c1af2a543ffcd67021d8b8c1d6
Jakub Myrta <mkuba2006@gmail.com>
2024-04-12 19:43:28 +0200

    3

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,15 +3,18 @@ const extras_button = document.getElementById("extras_button");
 const defaultColor = window.getComputedStyle(nav).backgroundColor;
 const logo = document.getElementById("logo_img");
 
-document.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        nav.style.backgroundColor = 'black';
-        extras_button.style.backgroundColor = 'black';
-        logo.style.backgroundColor = 'black';
-        logo.style.padding='14px 24px'
-        logo.style.borderRadius='32px'
-    } else {
-        nav.style.backgroundColor = defaultColor;
-        extras_button.style.backgroundColor = defaultColor;
-    }
-});
+if (window.innerWidth >"750px"){
+    document.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            nav.style.backgroundColor = 'black';
+            extras_button.style.backgroundColor = 'black';
+            logo.style.backgroundColor = 'black';
+            logo.style.padding='14px 24px'
+            logo.style.borderRadius='32px'
+        } else {
+            nav.style.backgroundColor = defaultColor;
+            extras_button.style.backgroundColor = defaultColor;
+        }
+    });
+    
+}
