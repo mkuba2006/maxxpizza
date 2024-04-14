013900b32ddcf1a128ad4e5f98773e0d6a64d23c
Jakub Myrta <mkuba2006@gmail.com>
2024-04-14 16:01:48 +0200

    14.04 2

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,17 +1,30 @@
-const nav = document.getElementById("links");
+const nav = document.getElementById("nav");
 const extras_button = document.getElementById("extras_button");
 const defaultColor = window.getComputedStyle(nav).backgroundColor;
 const logo = document.getElementById("logo_img");
+let main = document.getElementById('main').clientHeight;
 
 if (window.innerWidth >750){
     document.addEventListener("scroll", () => {
-        if (window.scrollY > 300) {
-            nav.style.backgroundColor = 'black';
-            extras_button.style.backgroundColor = 'black';
-            logo.style.backgroundColor = 'black';
-            logo.style.borderRadius='32px'
-        } else {
-            nav.style.backgroundColor = defaultColor;
+        // if (window.scrollY > 30) {
+        //     nav.style.backgroundColor = 'black';
+        //     extras_button.style.backgroundColor = 'black';
+        //     logo.style.backgroundColor = 'black';
+        //     logo.style.borderRadius='32px'
+        // } else {
+        //     nav.style.backgroundColor = defaultColor;
+        // }
+
+
+        // if(window.scrollY > main - 100){
+        //     nav.style.opacity= '0'
+        // }else{
+        //     nav.style.opacity= '1'
+        // }
+        if(window.scrollY > main - 100){
+            nav.style.transform= 'translate(-50%, -100px)'
+        }else{
+            nav.style.transform= 'translate(-50%, 0)'
         }
     });
     
